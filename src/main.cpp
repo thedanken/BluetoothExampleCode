a75ffda1e858e23503080ff90fc228ec56394648
Daniel Kennelty <dkennelty@gmail.com>
2023-04-20 11:41:30 -0400

    Defines for Nucleo F429ZI SPI6 pins

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@ Serial blue(p9, p10);          		// TX, RX
 #elif defined(TARGET_LPC4330_M4)
 Serial blue(P6_4, P6_5);         	// UART0_TX, UART0_RX
 //Serial blue(P2_3, P2_4);         	// UART3_TX, UART3_RX
+#elif defined(TARGET_NUCLEO_F429ZI)
+Serial blue(PG_14, PG_12);         	// SPI6_MOSI, SPI6_MISO
 #endif
  
 DigitalOut myled(LED1);
