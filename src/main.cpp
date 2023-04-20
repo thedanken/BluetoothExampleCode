4e947bf51a27c674513ee1afa1bbd44d46a1a16b
Daniel Kennelty <dkennelty@gmail.com>
2023-04-20 18:15:43 -0400

    Working example Uses LEDs on board TX RX require voltage division with HC-05 board

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,67 @@
 #include "mbed.h"
+#include "string.h"
+
+#define UART_RX PD_2
+#define UART_TX PC_12
+BufferedSerial pc(UART_TX, UART_RX, 9600);
+
+const int MAXBUFFSIZE = 32;
+#define UART5_TX PC_12
+#define UART5_RX PD_2
+
+BufferedSerial blue(UART5_TX, UART5_RX, 9600);
  
-Serial pc(USBTX, USBRX);
- 
-#if   defined(TARGET_LPC1768)
-Serial blue(p9, p10);          		// TX, RX
-//Serial blue(p13, p14);         	// TX, RX
-#elif defined(TARGET_LPC4330_M4)
-Serial blue(P6_4, P6_5);         	// UART0_TX, UART0_RX
-//Serial blue(P2_3, P2_4);         	// UART3_TX, UART3_RX
-#elif defined(TARGET_NUCLEO_F429ZI)
-Serial blue(PG_14, PG_12);         	// SPI6_MOSI, SPI6_MISO
-#endif
- 
-DigitalOut myled(LED1);
-DigitalOut myled4(LED4);
- 
+DigitalOut LedGreen(LED1);
+DigitalOut LedBlue(LED2);
+char buf[MAXBUFFSIZE] = {' '};
+
+void clearbuffer(void);
+
 int main() 
 {
-    blue.baud(115200);
-    pc.baud(115200);
-    pc.printf("Bluetooth Start\r\n");
-    
+  blue.set_format(
+    8,
+    BufferedSerial::None,
+    1
+  );
+  pc.set_format(
+    8,
+    BufferedSerial::None,
+     1
+  );
+  printf("Bluetooth Start\r\n");
+
+  char buf[MAXBUFFSIZE] = {0};  
+  string s;
     // echo back characters and toggle the LED
     while (1) 
     {
         if (blue.readable()) 
         {
-            pc.putc(blue.getc());
-            myled = !myled;
+          blue.read(buf, sizeof(buf));
+          printf(buf);
+          if(buf[0] == 'h')
+          {
+            LedGreen = !LedGreen;
+          }
+          else
+          {
+            LedBlue = !LedBlue;
+          }
+          clearbuffer();
         }
         if (pc.readable()) 
         {
-            blue.putc(pc.getc());
-            myled4 = !myled4;
+          pc.read(buf, sizeof(buf));
+          blue.write(buf, sizeof(buf));
+          
         }
     }
+}
+void clearbuffer()
+{
+  for(int i = 0; i< MAXBUFFSIZE; i++)
+  {
+    buf[i] = (char)0;
+  }
 }
